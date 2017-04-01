7720ef3d5b03ea971e4ebf165d45806cfc464b7f
trajepl <trajepl@gmail.com>
2017-04-01 14:36:22 +0800

    format broadcast and  trade

--- blockchain/module/broadcast.py
+++ blockchain/module/broadcast.py
@@ -1,4 +1,5 @@
 # broadcast msg
+import sys
 import time
 import random
 from threading import Timer
@@ -6,8 +7,9 @@ from threading import Timer
 import trade
 
 SEND_INTERVAL = 10
+TIMER_SEND  = None
 
-def broadcast_msg_timer(data, host, port):
+def broadcast_msg(data, host, port):
     '''
     : (host, port) broadcasts trasfer data to its first-step node.
     '''
@@ -16,43 +18,56 @@ def broadcast_msg_timer(data, host, port):
    
 def random_send(data, hosts_list):
     '''
-    : set fixed time (intercal 10s) to randomly selectting a source host sending data
+    : set fixed time (interval 10s) to randomly selectting a source host sending data
     '''
     is_continue = random.choice([True, False])
     if is_continue :
         src_hosts = random.choice(hosts_list)
         src_host = src_hosts[0]
         src_port = src_hosts[1]
-        print('    17: ' + str(src_hosts))
-        broadcast_msg_timer(data, src_host, src_port)
+        print('DEBUG 17 src_hosts: ' + str(src_hosts))
+        broadcast_msg(data, src_host, src_port)
     else:
         pass
-    timer = Timer(SEND_INTERVAL, random_send(data, hosts_list))
-    timer.start()
+    
+    global TIMER_SEND
+    TIMER_SEND = Timer(SEND_INTERVAL, random_send(data, hosts_list))
+    TIMER_SEND.start()
 
 def random_data(hosts_list):
     '''
-    : random trasfer data
+    : random trasfer data generator
     '''
     trade_double = random.sample(hosts_list, 2)
     return str(trade_double[0]) + ' trasfer * bitcoins to ' \
         + str(trade_double[1]) + '.'
 
-def start_broaccast(hosts_list):
+def start_broadcast(hosts_list):
     '''
     : start timer
     '''
     data = random_data(hosts_list)
-    print('    25: ' + data)
-    timer = Timer(0, random_send(data, hosts_list))
-    timer.start()
-    return timer
+    print('DEBUG 25 data: ' + data)
+
+    global TIMER_SEND
+    TIMER_SEND = Timer(0, random_send(data, hosts_list))
+    TIMER_SEND.start()
+
+
+# call by 'python broadcast hosts_list1 hosts_list2 ...'
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("error param.")
+    else:
+        hosts_list = sys.argv[1:]
+        start_broadcast(hosts_list)
+
 
 
 # # test
 # if __name__ == '__main__':
 #     hosts_list = [('127.0.0.1', 9000), ('127.0.0.1', 9001), ('127.0.0.1', 9002), ('127.0.0.1', 9003)]
-#     start_broaccast(hosts_list)
+#     start_broadcast(hosts_list)
 
 #     time.sleep(15)
 #     timer.cancle()
--- blockchain/module/trade.py
+++ blockchain/module/trade.py
@@ -7,31 +7,25 @@ import hashlib
 
 class Trade:
     def __init__(self, host, port_server):
+        '''
+        : init socket host(string) | port_server(int)
+        '''
         self.HOST = host
-        self.SOCKET_LIST = []
-        self.HOST_LIST = []
-        self.RECV_BUFFER = 4096
-        # self.PORT_SERVER = 8081
         self.PORT_SERVER = port_server
-        self.MAX_LEN_CONN = 10
-        self.TRANSACTION = set()
-        self.DATA = ""
 
-        # socket server
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # get the one-step node
-        self.get_socket_list('host/'+str(self.PORT_SERVER))
+        self.RECV_BUFFER = 4096
+        self.MAX_LEN_CONN = 10
+
+        self.SOCKET_LIST = []  # connection client socket_list
+        self.TRANSACTION = set() # transaction information
+        self.HOST_LIST = [] # one-step node
+        self.get_socket_list('host/'+str(self.PORT_SERVER)) # get the one-step node
+
+        self.DATA = "" 
 
     def set_data(self, data):
         if len(data) != 0:
             self.DATA = data
-
-    def set_host(self, host, port_server):
-        self.HOST = host
-        self.PORT_SERVER = int(port_server)
-
-    def set_max_len_conn(self, num):
-        self.MAX_LEN_CONN = num
 
     def get_socket_list(self, fn):
         with open(fn, 'r') as socket_list:
@@ -39,6 +33,10 @@ class Trade:
                 self.HOST_LIST.append(line)
 
     def start_server(self):
+        '''
+        : start socket server
+        '''
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket server
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.HOST, self.PORT_SERVER))
         self.server_socket.listen(self.MAX_LEN_CONN)
@@ -47,88 +45,91 @@ class Trade:
         print('Start trade server.')
 
     def start_send(self):
+        '''
+        : start socket client
+        '''
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.settimeout(2)
 
         for host in self.HOST_LIST:
-            host = host.split(' ')
+            host = host.split(' ') # host: [ip(string), port(string)]
+            host[1] = int(host[1]) 
 
-            # cluster debug
-            host[1] = int(host[1])
-            # host[0] == self.HOST
+            if host[1] == self.PORT_SERVER and host[0] == self.HOST: 
+                continue # except itself
+            
+            while True:
+                print("$ [2 send to]: %s" % host)
+                try:
+                    self.client_socket.connect(tuple(host))
+                    self.client_socket.send(self.DATA.encode())
+                except Exception as e:
+                    print('! 59: %s %s' % (e, host))
 
-            if host[1] == self.PORT_SERVER:
-                continue
-
-            print("$ [send to]: %s" % host)
-            try:
-                self.client_socket.connect(tuple(host))
-                self.client_socket.send(self.DATA.encode())
-            except Exception as e:
-                print('! line 59: %s %s' % (e, host))
-            try:
-                check_code = self.client_socket.recv(self.RECV_BUFFER)
-                if self.check(check_code, self.client_socket):
-                    print('> [recv] right info: %s.' % host)
-                    self.client_socket.send(b'success')
-                else:
-                    print('> [recv] wrong info: %s.' % host)
-                    print('> [resend] to %s.' % host)
-            except Exception as e:
-                print('! line 67: %s %s' % (e, host))
-            finally:
-                # close the client
-                self.client_socket.close()
-
+                try:
+                    check_code = self.client_socket.recv(self.RECV_BUFFER)
+                    if self.check(check_code, self.client_socket):
+                        print('> [3 recv] right info: %s.' % host)
+                        self.client_socket.send(b'success')
+                    else:
+                        print('> [3 recv] wrong info: %s.' % host)
+                        print('> [4 resend] to %s.' % host)
+                        continue # resend self.DATA
+                except Exception as e:
+                    print('! 67: %s %s' % (e, host))
+                finally:
+                    self.client_socket.close()
+                    break
 
     def check(self, check_code, sock):
+        '''
+        : check information which server receives
+        '''
         origin_data = ''
         for item in self.DATA:
             origin_data += item
-
         hash_code = hashlib.sha256(origin_data.encode()).hexdigest().encode()
-
-        if hash_code == check_code:
-            sock.send(b'True')
-            return True
-        else:
-            sock.send(b'False')
-            return False
+    
+        return True if hash_code == check_code else False
 
     def send(self, data):
+        '''
+        : set data information then call self.start_send to send data
+        '''
         self.set_data(data)
-        print('> [sending] %s' % self.DATA)
+        print('> [1 sending] %s' % self.DATA)
         self.start_send()
 
     def receive(self):
+        '''
+        : receive rules in server socket
+        '''
         while True:
             ready_to_read, read_to_write, in_error = select.select(self.SOCKET_LIST, [], [], 0)
             for sock in ready_to_read:
-                # a new connection request received
-                if sock == self.server_socket:
+                if sock == self.server_socket: 
                     sockfd, addr = self.server_socket.accept()
-                    self.SOCKET_LIST.append(sockfd)
+                    self.SOCKET_LIST.append(sockfd) # a new connection request received
                 else:
                     try:
-                        data_bytes = sock.recv(self.RECV_BUFFER)
+                        data_bytes = sock.recv(self.RECV_BUFFER) # receive data
                         data = data_bytes.decode()
                         check_code = hashlib.sha256(data_bytes).hexdigest().encode()
 
                         if len(data) != 0:
                             sock.send(check_code)
                             print('> [recv](%s@%s) receives info.' % addr)
-                            print('> ' + data + ':' + check_code.decode())
-                            check_data = sock.recv(self.RECV_BUFFER).decode()
+                            check_data = sock.recv(self.RECV_BUFFER).decode() # check status
                             if check_code == 'success':
                                 self.TRANSACTION.add(data)
                                 if len(self.SOCKET_LIST) != 0:
                                     self.DATA = data
-                                    self.start_send()
+                                    self.start_send() # transmit data to one-step node
                         else:
                             print('> [recv](%s@ %s) is offline.' % addr)
                         self.SOCKET_LIST.remove(sock)
                     except Exception as e:
-                        print(e)
+                        print('! 132: %s.' % e)
                         self.SOCKET_LIST.remove(sock)
         self.server_socket.close()
 
@@ -144,6 +145,6 @@ def start_server(host, port):
     else:
         print('wrong parm.')
 
-
+# # test
 # if __name__ == '__main__':
 #     start_server('127.0.0.1', 8000)
